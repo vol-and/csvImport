b81ffb5fa8fdfc5adba2fe4acb7cbaa62d7abdb9
vol-and <40835576+vol-and@users.noreply.github.com>
2021-09-26 16:14:13 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+const DB_NAME = '';
+const DB_HOST = 'localhost';
+const DB_PASS = '';
+const DB_USER = '';
+const TABLE_NAME = 'users';
+
+const DOMAIN = '';
+const INDEX_LINK = DOMAIN . 'index.php';
+const ADMIN_MAIL = '';
+
+const COLS_QUANTITY = 4;
